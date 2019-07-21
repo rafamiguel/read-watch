3ebbc0af21dd2ae25a7d7e646fd3d552d1ea131d
skillerhero <skillerhero@users.noreply.github.com>
2019-07-21 17:11:46 -0500

    Proyecto añadido a GitHub

--- app/src/main/java/estrada/leon/rafael/readwatch/MainActivity.java
+++ app/src/main/java/estrada/leon/rafael/readwatch/MainActivity.java
@@ -2,9 +2,16 @@ package estrada.leon.rafael.readwatch;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageView logo;
+    EditText txtUsuario,txtContra;
+    Button btnEntrar;
+    TextView lblContra,lblCuenta,lblRegistrar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
