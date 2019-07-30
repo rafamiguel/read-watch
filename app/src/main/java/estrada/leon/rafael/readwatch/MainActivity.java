7a19447dc8027bfd5e142f8f4054fdf50742daca
miguelangelluis <52984840+miguelangelluis@users.noreply.github.com>
2019-07-30 12:34:47 -0500

    Primera pantalla terminada

--- app/src/main/java/estrada/leon/rafael/readwatch/MainActivity.java
+++ app/src/main/java/estrada/leon/rafael/readwatch/MainActivity.java
@@ -2,6 +2,7 @@ package estrada.leon.rafael.readwatch;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -16,5 +17,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        logo = findViewById(R.id.logo);
+        txtUsuario = findViewById(R.id.txtUsuario);
+        txtContra = findViewById(R.id.txtContra);
+        btnEntrar = findViewById(R.id.btnEntrar);
+        lblContra = findViewById(R.id.lblContra);
+        lblCuenta = findViewById(R.id.lblCuenta);
+        lblRegistrar = findViewById(R.id.lblRegistrar);
+
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
