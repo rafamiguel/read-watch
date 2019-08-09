e9c02b3c8a73aaf2c809ed902e2ad53e705e6d50
miguelangelluis <52984840+miguelangelluis@users.noreply.github.com>
2019-08-09 13:42:10 -0500

    Alert pagina 29, contraseña olvidada

---
+++ app/src/main/java/estrada/leon/rafael/readwatch/BtnOlvidoContrasena.java
@@ -0,0 +1,30 @@
+package estrada.leon.rafael.readwatch;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class BtnOlvidoContrasena extends AppCompatActivity {
+    Button btnContrasena;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_btn_olvido_contrasena);
+        btnContrasena = findViewById(R.id.btnContrasena);
+        btnContrasena.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDialog();
+
+            }
+        });
+    }
+
+    public void openDialog() {
+        DialogCorreoOlvidado dialogCorreoOlvidado = new DialogCorreoOlvidado();
+        dialogCorreoOlvidado.show(getSupportFragmentManager(), "Example");
+    }
+}
