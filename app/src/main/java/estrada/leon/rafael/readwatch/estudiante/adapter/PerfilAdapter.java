000460d8c45ee2f1fc3b73f2aa46a1990ab933e4
skillerhero <skillerhero@users.noreply.github.com>
2019-08-12 00:44:04 -0500

    Hice la vista de Perfil y puse la carpeta de dialogs como resources folder porque era una carpeta normal

--- app/src/main/java/estrada/leon/rafael/readwatch/estudiante/adapter/PerfilAdapter.java
+++ app/src/main/java/estrada/leon/rafael/readwatch/estudiante/adapter/PerfilAdapter.java
@@ -1,4 +1,171 @@
 package estrada.leon.rafael.readwatch.estudiante.adapter;
 
-public class PerfilAdapter {
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
+import estrada.leon.rafael.readwatch.R;
+import estrada.leon.rafael.readwatch.estudiante.interfaces.Item;
+import estrada.leon.rafael.readwatch.estudiante.pojo.Documentos;
+import estrada.leon.rafael.readwatch.estudiante.pojo.Videos;
+
+public class PerfilAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    private Context context;
+    private List<Item> list;
+    private OnPerfilListener onPerfilListener;
+    private final int documento=1;
+    private final int video=2;
+
+    public PerfilAdapter(Context context,List<Item> list,OnPerfilListener onPerfilListener){
+        this.context=context;
+        this.list=list;
+        this.onPerfilListener=onPerfilListener;
+    }
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+        RecyclerView.ViewHolder viewHolder;
+        View view;
+        switch (viewType){
+            case documento:{
+                view= LayoutInflater.from(context).inflate(R.layout.documento_fav,viewGroup,false);
+                viewHolder=new DocumentosViewHolder(view,onPerfilListener);
+                break;
+            }
+            case video:{
+                view=LayoutInflater.from(context).inflate(R.layout.video_fav,viewGroup,false);
+                viewHolder=new VideosViewHolder(view,onPerfilListener);
+                break;
+            }
+            default:
+                view=LayoutInflater.from(context).inflate(R.layout.video_fav,viewGroup,false);
+                viewHolder=new VideosViewHolder(view,onPerfilListener);
+                break;
+        }
+        RecyclerView.LayoutParams lp = new RecyclerView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        view.setLayoutParams(lp);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int position) {
+        switch(getItemViewType(position)){
+            case documento: {
+                Documentos documento = (Documentos) list.get(position);
+                DocumentosViewHolder documentosViewHolder = (DocumentosViewHolder) viewHolder;
+                documentosViewHolder.lblPerfil.setText(documento.getPerfil());
+                documentosViewHolder.lblDescripcion.setText(documento.getDescripcion());
+                break;
+            }
+            case video:{
+                Videos video=(Videos)list.get(position);
+                VideosViewHolder videosViewHolder=(VideosViewHolder) viewHolder;
+                videosViewHolder.lblPerfil.setText(video.getPerfil());
+                videosViewHolder.lblDescripcion.setText(video.getDescripcion());
+                String uri = video.getRutaImagen();
+                int imageResource = context.getResources().getIdentifier(uri,null,context.getPackageName());
+                videosViewHolder.btnMiniatura.setImageResource(imageResource);
+                break;
+            }
+            default:
+                Videos video=(Videos)list.get(position);
+                VideosViewHolder videosViewHolder=(VideosViewHolder) viewHolder;
+                videosViewHolder.lblPerfil.setText(video.getPerfil());
+                videosViewHolder.lblDescripcion.setText(video.getDescripcion());
+                String uri = video.getRutaImagen();
+                int imageResource = context.getResources().getIdentifier(uri,null,context.getPackageName());
+                videosViewHolder.btnMiniatura.setImageResource(imageResource);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class VideosViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        private ImageView btnMiniatura;
+        private Button btnFavorito;
+        private TextView lblPerfil,lblDescripcion;
+        private OnPerfilListener onPerfilListener;
+
+        public VideosViewHolder(@NonNull View itemView,OnPerfilListener onPerfilListener) {
+            super(itemView);
+            btnMiniatura=itemView.findViewById(R.id.btnMiniatura);
+            lblPerfil=itemView.findViewById(R.id.lblPerfil);
+            lblDescripcion=itemView.findViewById(R.id.lblDescripcion);
+            btnFavorito=itemView.findViewById(R.id.btnFavorito);
+            this.onPerfilListener=onPerfilListener;
+            btnFavorito.setOnClickListener(this);
+            btnMiniatura.setOnClickListener(this);
+            lblPerfil.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.btnMiniatura:
+                    onPerfilListener.onPerfilClick(getAdapterPosition(),list, Toast.makeText(context,"Miniatura",Toast.LENGTH_SHORT));
+                    break;
+                case R.id.lblPerfil:
+                    onPerfilListener.onPerfilClick(getAdapterPosition(),list,Toast.makeText(context,"Perfil",Toast.LENGTH_SHORT));
+                    break;
+                case R.id.btnFavorito:
+                    onPerfilListener.onPerfilClick(getAdapterPosition(),list,Toast.makeText(context,"Favorito",Toast.LENGTH_SHORT));
+                    break;
+            }
+        }
+    }
+
+    public class DocumentosViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        ImageView btnDocumento;
+        Button btnFavorito;
+        TextView lblPerfil,lblDescripcion;
+        OnPerfilListener onPerfilListener;
+
+        private DocumentosViewHolder(@NonNull View itemView,OnPerfilListener onPerfilListener) {
+            super(itemView);
+            btnDocumento= itemView.findViewById(R.id.btnDocumento);
+            lblPerfil=itemView.findViewById(R.id.lblPerfil);
+            lblDescripcion=itemView.findViewById(R.id.lblDescripcion);
+            btnFavorito=itemView.findViewById(R.id.btnFavorito);
+            this.onPerfilListener=onPerfilListener;
+            btnDocumento.setOnClickListener(this);
+            lblPerfil.setOnClickListener(this);
+            btnFavorito.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.btnDocumento:
+                    onPerfilListener.onPerfilClick(getAdapterPosition(),list,Toast.makeText(context,"Miniatura documento",Toast.LENGTH_SHORT));
+                    break;
+                case R.id.lblPerfil:
+                    onPerfilListener.onPerfilClick(getAdapterPosition(),list,Toast.makeText(context,"Perfil",Toast.LENGTH_SHORT));
+                    break;
+                case R.id.btnFavorito:
+                    onPerfilListener.onPerfilClick(getAdapterPosition(),list,Toast.makeText(context,"Favorito",Toast.LENGTH_SHORT));
+                    break;
+            }
+        }
+    }
+
+    @Override
+    public int getItemViewType(int posicion){
+        return list.get(posicion).getViewType();
+    }
+    public interface OnPerfilListener {
+        void onPerfilClick(int position, List<Item> lista, Toast toast);
+    }
+
 }
--- app/src/main/java/estrada/leon/rafael/readwatch/estudiante/fragment/Perfil.java
+++ app/src/main/java/estrada/leon/rafael/readwatch/estudiante/fragment/Perfil.java
@@ -1,31 +1,49 @@
 package estrada.leon.rafael.readwatch.estudiante.fragment;
 
+import android.app.Activity;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import estrada.leon.rafael.readwatch.R;
+import estrada.leon.rafael.readwatch.estudiante.adapter.PerfilAdapter;
+import estrada.leon.rafael.readwatch.estudiante.interfaces.Item;
+import estrada.leon.rafael.readwatch.estudiante.interfaces.iComunicacionFragments;
+import estrada.leon.rafael.readwatch.estudiante.pojo.Documentos;
+import estrada.leon.rafael.readwatch.estudiante.pojo.Videos;
 
-public class Perfil extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+public class Perfil extends Fragment implements PerfilAdapter.OnPerfilListener {
+    List<Item> list;
+    private iComunicacionFragments interfaceFragments;
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
     private OnFragmentInteractionListener mListener;
 
     public Perfil() {
-        // Required empty public constructor
     }
-
+    void cargarDatos(){
+        list=new ArrayList<>();
+        for(int i=1;i<11;i++){
+            list.add(new Documentos("perfil"+i,"Documento"+i,"@drawable/btnDocumento"));
+            list.add(new Videos("perfil"+i,"video"+i,"@drawable/miniatura"));
+        }
+    }
     public static Perfil newInstance(String param1, String param2) {
         Perfil fragment = new Perfil();
         Bundle args = new Bundle();
@@ -47,8 +65,23 @@ public class Perfil extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_perfil, container, false);
+        ImageView fotoPerfil;
+        TextView lblNombreApellidos,lblDescripcion,lblCelular;
+        RecyclerView recyclerPerfil;
+        PerfilAdapter perfilAdapter;
+        View view;
+        view = inflater.inflate(R.layout.fragment_perfil, container, false);
+        fotoPerfil=view.findViewById(R.id.fotoPerfil);
+        lblNombreApellidos=view.findViewById(R.id.lblNombreApellidos);
+        lblDescripcion=view.findViewById(R.id.lblDescripcion);
+        lblCelular=view.findViewById(R.id.lblCelular);
+        fotoPerfil.setImageResource(R.drawable.profilepic);
+        recyclerPerfil=view.findViewById(R.id.recyclerPerfil);
+        recyclerPerfil.setLayoutManager(new LinearLayoutManager(getContext(),LinearLayoutManager.VERTICAL,false));
+        cargarDatos();
+        perfilAdapter=new PerfilAdapter(getContext(),list,this);
+        recyclerPerfil.setAdapter(perfilAdapter);
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -60,7 +93,12 @@ public class Perfil extends Fragment {
 
     @Override
     public void onAttach(Context context) {
+        Activity activity;
         super.onAttach(context);
+        if (context instanceof Activity) {
+            activity= (Activity) context;
+            interfaceFragments=(iComunicacionFragments)activity;
+        }
         if (context instanceof OnFragmentInteractionListener) {
             mListener = (OnFragmentInteractionListener) context;
         } else {
@@ -73,6 +111,11 @@ public class Perfil extends Fragment {
     public void onDetach() {
         super.onDetach();
         mListener = null;
+    }
+
+    @Override
+    public void onPerfilClick(int position, List<Item> lista, Toast toast) {
+        interfaceFragments.onClickDocFavHolder(toast);
     }
 
     public interface OnFragmentInteractionListener {
