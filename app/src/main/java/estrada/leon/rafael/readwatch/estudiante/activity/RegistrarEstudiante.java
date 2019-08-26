0d8d778cbbfd55caf47b0ba802df9a6aee749fb5
miguelangelluis <52984840+miguelangelluis@users.noreply.github.com>
2019-08-25 19:18:32 -0500

    Conexiòn a la base de datos y registrar estudiante

--- app/src/main/java/estrada/leon/rafael/readwatch/estudiante/activity/RegistrarEstudiante.java
+++ app/src/main/java/estrada/leon/rafael/readwatch/estudiante/activity/RegistrarEstudiante.java
@@ -1,5 +1,6 @@
 package estrada.leon.rafael.readwatch.estudiante.activity;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,14 +8,27 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 import estrada.leon.rafael.readwatch.R;
 
-public class RegistrarEstudiante extends AppCompatActivity {
+public class RegistrarEstudiante extends AppCompatActivity implements Response.Listener<JSONObject>, Response.ErrorListener {
     TextView lblTitulo;
     EditText txtNombre, txtApellido, txtCorreo, txtContrasena, txtTelefono, txtDescripcion;
     Button btnEntrar;
     ImageView btnFoto;
+    ProgressDialog progreso;
+    RequestQueue request;
+    JsonObjectRequest jsonObjectRequest;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +43,8 @@ public class RegistrarEstudiante extends AppCompatActivity {
         btnFoto = findViewById(R.id.btnFoto);
         btnEntrar = findViewById(R.id.btnEntrar);
 
+        request = Volley.newRequestQueue(getApplicationContext());
+
         btnFoto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -39,8 +55,42 @@ public class RegistrarEstudiante extends AppCompatActivity {
         btnEntrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                cargarWebService();
             }
         });
+    }
+
+    private void cargarWebService() {
+        progreso = new ProgressDialog(this);
+        progreso.setMessage("Cargando...");
+        progreso.show();
+        String url = "http://192.168.1.67/randwBDRemota/registroUsuario.php?txtCorreo="+txtCorreo.getText().toString()+
+                "&txtContrasena="+txtContrasena.getText().toString()+
+                "&txtNombre="+txtNombre.getText().toString()+
+                "&txtApellido="+txtApellido.getText().toString()+
+                "&txtTelefono="+txtTelefono.getText().toString()+
+                "&txtDescripcion="+txtDescripcion.getText().toString()+"";
+        url=url.replace(" ", "%20");
+        jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, this, this);
+        request.add(jsonObjectRequest);
+    }
+
+    @Override
+    public void onErrorResponse(VolleyError error) {
+        progreso.hide();
+        Toast.makeText(getApplicationContext(), "No se pudo registrar"+error.toString(), Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onResponse(JSONObject response) {
+        Toast.makeText(getApplicationContext(), "Se ha registrado exitosamente", Toast.LENGTH_SHORT).show();
+        progreso.hide();
+        txtDescripcion.setText("");
+        txtTelefono.setText("");
+        txtApellido.setText("");
+        txtNombre.setText("");
+        txtContrasena.setText("");
+        txtCorreo.setText("");
     }
 }
