b72b49a622d09deb1e6c7ad06325ca8b7099f484
miguelangelluis <52984840+miguelangelluis@users.noreply.github.com>
2019-08-29 01:17:06 -0500

    Ya muestra la informacion del administrador

---
+++ app/src/main/java/estrada/leon/rafael/readwatch/entidades/Admin.java
@@ -0,0 +1,40 @@
+package estrada.leon.rafael.readwatch.entidades;
+
+public class Admin {
+    private String nombre;
+    private String apellidos;
+    private String correo;
+    private String contrasena;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+}
