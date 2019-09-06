cc96298c226c55866589e911bcbb411b31463652
miguelangelluis <52984840+miguelangelluis@users.noreply.github.com>
2019-09-06 00:35:33 -0500

    Subir documento ya funciona para cargar el documento

--- app/src/main/java/estrada/leon/rafael/readwatch/estudiante/dialog/Dialog_Recuadro_Subir_documento.java
+++ app/src/main/java/estrada/leon/rafael/readwatch/estudiante/dialog/Dialog_Recuadro_Subir_documento.java
@@ -2,19 +2,26 @@ package estrada.leon.rafael.readwatch.estudiante.dialog;
 
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatDialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.TextView;
 
+import estrada.leon.rafael.readwatch.MainFileManager;
 import estrada.leon.rafael.readwatch.R;
+import estrada.leon.rafael.readwatch.general.activity.MainActivity;
 
 public class Dialog_Recuadro_Subir_documento extends AppCompatDialogFragment {
+    Intent entrar;
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        TextView lblElegirDocumento;
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
 
         LayoutInflater inflater = getActivity().getLayoutInflater();
         View view = inflater.inflate(R.layout.estilo_recuadro_subir_documento, null);
@@ -32,6 +39,15 @@ public class Dialog_Recuadro_Subir_documento extends AppCompatDialogFragment {
 
                     }
                 });
+
+        lblElegirDocumento = view.findViewById(R.id.lblElegirDocumento);
+        lblElegirDocumento.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                entrar = new Intent(getContext(), MainFileManager.class);
+                startActivity(entrar);
+            }
+        });
         return builder.create();
     }
 }
