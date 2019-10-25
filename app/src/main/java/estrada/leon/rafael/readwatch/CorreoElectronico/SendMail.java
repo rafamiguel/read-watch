3d38d55f0cfdbcf3539995574070ed66118a1715
miguelangelluis <52984840+miguelangelluis@users.noreply.github.com>
2019-10-24 19:32:14 -0500

    Ya modifica la contraseña que se envia al correo electronico

--- app/src/main/java/estrada/leon/rafael/readwatch/CorreoElectronico/SendMail.java
+++ app/src/main/java/estrada/leon/rafael/readwatch/CorreoElectronico/SendMail.java
@@ -49,7 +49,7 @@ public class SendMail extends AsyncTask<Void,Void,Void> {
         super.onPostExecute(aVoid);
         progressDialog.dismiss();
         //Showing a success message
-        Toast.makeText(context,"Message Sent",Toast.LENGTH_LONG).show();
+        //Toast.makeText(context,"Message Sent",Toast.LENGTH_LONG).show();
     }
 
     @Override
--- app/src/main/java/estrada/leon/rafael/readwatch/estudiante/dialog/DialogContrasenaOlvidada.java
+++ app/src/main/java/estrada/leon/rafael/readwatch/estudiante/dialog/DialogContrasenaOlvidada.java
@@ -1,6 +1,8 @@
 package estrada.leon.rafael.readwatch.estudiante.dialog;
 
 import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
@@ -9,6 +11,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.Random;
 
@@ -19,11 +32,17 @@ public class DialogContrasenaOlvidada extends AppCompatDialogFragment {
     private final String caracteres = "abcdefghijklmnñopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZ1234567890!#$%&/()=?¡¿'|°";
     private String nuevaContraseña="", correo="";
     private Random rand;
+    private Context context;
+    ProgressDialog progreso;
+    JsonObjectRequest jsonObjectRequest;
+    RequestQueue request;
+
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         final EditText txtCorreo;
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-
+        request= Volley.newRequestQueue(getActivity());
+        context = DialogContrasenaOlvidada.this.getContext();
         LayoutInflater inflater = getActivity().getLayoutInflater();
         View view = inflater.inflate(R.layout.dialog_olvido_contrasena, null);
         txtCorreo=view.findViewById(R.id.txtContrasena);
@@ -38,6 +57,7 @@ public class DialogContrasenaOlvidada extends AppCompatDialogFragment {
                                 "' está registrado se enviará la nueva contraseña. Espere unos minutos.",
                                 Toast.LENGTH_LONG).show();
                         correo=txtCorreo.getText().toString();
+
                         nuevaContraseña="";
                         rand = new Random();
                         for(int iterador=0;iterador<15;iterador++){
@@ -45,10 +65,12 @@ public class DialogContrasenaOlvidada extends AppCompatDialogFragment {
                         }
 
                         //Creating SendMail object
-                        SendMail sm = new SendMail(getContext(), correo, "Reestablecimiento de contraseña", "Nueva contraseña:\n"+nuevaContraseña);
+                        SendMail sm = new SendMail(context, correo, "Reestablecimiento de contraseña", "Nueva contraseña:\n"+nuevaContraseña);
 
                         //Executing sendmail to send email
                         sm.execute();
+
+                        verificarExistencia(correo, nuevaContraseña);
                     }
                 })
                 .setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
@@ -58,5 +80,45 @@ public class DialogContrasenaOlvidada extends AppCompatDialogFragment {
                     }
                 });
         return builder.create();
+    }
+
+    private void verificarExistencia(String correo, String nuevaContraseña) {
+        String url;
+        url = "https://readandwatch.herokuapp.com/php/updateContrasenaCorreo.php?" +
+                "correo="+correo+"&contrasena="+nuevaContraseña;
+        url=url.replace(" ", "%20");
+        jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                int success;
+                JSONArray json;
+                JSONObject jsonObject=null;
+                json = response.optJSONArray("usuario");
+
+                    try {
+                        jsonObject = json.getJSONObject(0);
+                        success = jsonObject.getInt("success");
+
+                        if (success==0){
+                           // Toast.makeText(context, "No se encontro el correo", Toast.LENGTH_SHORT).show();
+
+                        }
+                        else {
+                          //  Toast.makeText(context, "Mensaje enviado", Toast.LENGTH_SHORT).show();
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        request.add(jsonObjectRequest);
     }
 }
