4d62eec81689c650d20b3b2bdcf613373c17b1e4
miguelangelluis <52984840+miguelangelluis@users.noreply.github.com>
2019-10-26 23:48:29 -0500

    prueba

--- app/src/main/java/estrada/leon/rafael/readwatch/MainVideos.java
+++ app/src/main/java/estrada/leon/rafael/readwatch/MainVideos.java
@@ -1,13 +1,29 @@
 package estrada.leon.rafael.readwatch;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.Vector;
 
 public class MainVideos extends AppCompatActivity {
+    ProgressDialog progreso;
+    JsonObjectRequest jsonObjectRequest;
+    RequestQueue request;
 
     RecyclerView recyclerView;
     Vector<YouTubeVideos> youtubeVideos = new Vector<YouTubeVideos>();
@@ -16,19 +32,51 @@ public class MainVideos extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_videos);
+        request= Volley.newRequestQueue(getApplicationContext());
 
         recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         recyclerView.setHasFixedSize(true);
         recyclerView.requestFocus();
         recyclerView.setLayoutManager( new LinearLayoutManager(this));
 
-        youtubeVideos.add( new YouTubeVideos("<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/9DnAomm5an4\" frameborder=\"0\" allowfullscreen></iframe>") );
-        youtubeVideos.add( new YouTubeVideos("<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/P1LiOhI14fc\" frameborder=\"0\" allowfullscreen></iframe>") );
-        youtubeVideos.add( new YouTubeVideos("<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/fidtFOnY1V4\" frameborder=\"0\" allowfullscreen></iframe>") );
-        youtubeVideos.add( new YouTubeVideos("<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/8FVwe_F7na8\" frameborder=\"0\" allowfullscreen></iframe>") );
-        youtubeVideos.add( new YouTubeVideos("<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/uhQ7mh_o_cM\" frameborder=\"0\" allowfullscreen></iframe>") );
+        progreso = new ProgressDialog(getApplicationContext());
+        progreso.setMessage("Cargando...");
+        progreso.show();
+        String url;
+        url = "https://readandwatch.herokuapp.com/php/buscarRutaVideo.php";
+        url=url.replace(" ", "%20");
+        jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                String ruta;
+                JSONArray json;
+                JSONObject jsonObject=null;
+                json = response.optJSONArray("usuario");
+                for(int i=0;i<json.length();i++) {
+                    try {
+                        jsonObject = json.getJSONObject(i);
+                        ruta = jsonObject.getString("ruta");
+                        String[] parts = ruta.split("=");
+                        ruta = parts[1];
+                        youtubeVideos.add( new YouTubeVideos("<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/"+ruta+"\" frameborder=\"0\" allowfullscreen></iframe>") );
+                        VideosYouTubeAdapter videoAdapter = new VideosYouTubeAdapter(youtubeVideos);
+                        recyclerView.setAdapter(videoAdapter);
 
-        VideosYouTubeAdapter videoAdapter = new VideosYouTubeAdapter(youtubeVideos);
-        recyclerView.setAdapter(videoAdapter);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+                progreso.hide();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                progreso.hide();
+                Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        request.add(jsonObjectRequest);
+
     }
 }
