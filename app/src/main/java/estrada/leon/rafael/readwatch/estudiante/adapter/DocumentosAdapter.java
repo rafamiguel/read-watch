679d72b5a2388087a3b5bd35ae84b3de2e37f243
skillerhero <skillerhero@users.noreply.github.com>
2019-11-04 03:58:07 -0600

    Arregle un problema con el merge por haber modificado el archivo ambos.

--- app/src/main/java/estrada/leon/rafael/readwatch/estudiante/adapter/DocumentosAdapter.java
+++ app/src/main/java/estrada/leon/rafael/readwatch/estudiante/adapter/DocumentosAdapter.java
@@ -2,16 +2,16 @@ package estrada.leon.rafael.readwatch.estudiante.adapter;
 
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.media.Image;
+import android.graphics.Canvas;
+import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -26,11 +26,22 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.File;
 import java.util.List;
 
-import estrada.leon.rafael.readwatch.estudiante.fragment.MainComentario;
 import estrada.leon.rafael.readwatch.estudiante.pojo.Documentos;
 import estrada.leon.rafael.readwatch.R;
+
+import com.github.barteksc.pdfviewer.PDFView;
+import com.github.barteksc.pdfviewer.listener.OnDrawListener;
+import com.github.barteksc.pdfviewer.listener.OnPageChangeListener;
+import com.github.barteksc.pdfviewer.listener.OnPageErrorListener;
+import com.github.barteksc.pdfviewer.listener.OnRenderListener;
+import com.github.barteksc.pdfviewer.listener.OnTapListener;
+import com.krishna.fileloader.FileLoader;
+import com.krishna.fileloader.listener.FileRequestListener;
+import com.krishna.fileloader.pojo.FileResponse;
+import com.krishna.fileloader.request.FileLoadRequest;
 
 public class DocumentosAdapter extends RecyclerView.Adapter<DocumentosAdapter.ViewHolder>{
 
@@ -40,6 +51,7 @@ public class DocumentosAdapter extends RecyclerView.Adapter<DocumentosAdapter.Vi
     private int[] idUsuarioVidDoc;
     private int[] idUsuarioVidDocFav;
     Intent entrar;
+    PDFView pdfView;
     JsonObjectRequest jsonObjectRequest;
     RequestQueue request;;
     String nombre;
@@ -65,13 +77,50 @@ public class DocumentosAdapter extends RecyclerView.Adapter<DocumentosAdapter.Vi
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+        FileLoader.with(context).load("https://readandwatch.000webhostapp.com/archivos/Mi_sistema_Nimzovith.pdf").fromDirectory("PDFFiles",FileLoader.DIR_EXTERNAL_PUBLIC).asFile(new FileRequestListener<File>() {
+            @Override
+            public void onLoad(FileLoadRequest request, FileResponse<File> response) {
+                File pdf = response.getBody();
+                pdfView.fromFile(pdf).password(null).defaultPage(0).enableSwipe(true).swipeHorizontal(false).enableDoubletap(true).onDraw(new OnDrawListener() {
+                    @Override
+                    public void onLayerDrawn(Canvas canvas, float pageWidth, float pageHeight, int displayedPage) {
+                    }
+                }).onDrawAll(new OnDrawListener() {
+                    @Override
+                    public void onLayerDrawn(Canvas canvas, float pageWidth, float pageHeight, int displayedPage) {
+                    }
+                }).onPageError(new OnPageErrorListener() {
+                    @Override
+                    public void onPageError(int page, Throwable t) {
+                    }
+                }).onPageChange(new OnPageChangeListener() {
+                    @Override
+                    public void onPageChanged(int page, int pageCount) {
+                    }
+                }).onTap(new OnTapListener() {
+                    @Override
+                    public boolean onTap(MotionEvent e) {
+                        return true;
+                    }
+                }).onRender(new OnRenderListener() {
+                    @Override
+                    public void onInitiallyRendered(int nbPages, float pageWidth, float pageHeight) {
+                        pdfView.fitToWidth();
+                    }
+                }).enableAnnotationRendering(true).invalidPageColor(Color.WHITE).load();
+            }
+            @Override
+            public void onError(FileLoadRequest request, Throwable t) {
+                Toast.makeText(context, t.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
         Documentos documento=documentosList.get(i);
         obtenerNombre(documento.getPerfil(), viewHolder);
-       // viewHolder.lblPerfil.setText(documento.getPerfil());
+        // viewHolder.lblPerfil.setText(documento.getPerfil());
         viewHolder.lblDescripcion.setText(documento.getDescripcion());
-        String uri = documento.getRutaImagen();
-        int imageResource = context.getResources().getIdentifier(uri,null,context.getPackageName());
-        viewHolder.btnDocumento.setImageResource(imageResource);
+        // String uri = documento.getRutaImagen();
+        // int imageResource = context.getResources().getIdentifier(uri,null,context.getPackageName());
+        // viewHolder.btnDocumento.setImageResource(imageResource);
         if(idUsuarioVidDocFav!=null){
             for (int j = 0; j < idUsuarioVidDocFav.length; j++) {
                 if (idUsuarioVidDocFav[j] == documento.getIdVidDoc()) {
@@ -83,18 +132,18 @@ public class DocumentosAdapter extends RecyclerView.Adapter<DocumentosAdapter.Vi
             }
 
         }
-            if (idUsuarioVidDoc!=null) {
-                for (int j = 0; j < idUsuarioVidDoc.length; j++) {
-                    if (idUsuarioVidDoc[j] == documento.getIdVidDoc()) {
-                        viewHolder.btnOpcion.setVisibility(View.VISIBLE);
-                        break;
-                    } else {
-                        viewHolder.btnOpcion.setVisibility(View.GONE);
-                    }
+        if (idUsuarioVidDoc!=null) {
+            for (int j = 0; j < idUsuarioVidDoc.length; j++) {
+                if (idUsuarioVidDoc[j] == documento.getIdVidDoc()) {
+                    viewHolder.btnOpcion.setVisibility(View.VISIBLE);
+                    break;
+                } else {
+                    viewHolder.btnOpcion.setVisibility(View.GONE);
                 }
-            }else{
-                viewHolder.btnOpcion.setVisibility(View.GONE);
             }
+        }else{
+            viewHolder.btnOpcion.setVisibility(View.GONE);
+        }
     }
 
     private void obtenerNombre(String idUsuario, final ViewHolder viewHolder) {
@@ -139,21 +188,23 @@ public class DocumentosAdapter extends RecyclerView.Adapter<DocumentosAdapter.Vi
         private TextView lblDescripcion,lblPerfil;
         private Button btnAdvertencia,btnFavorito,btnOpcion;
         private EditText txtComentario;
-        private ImageView btnDocumento;
+        //private ImageView btnDocumento;
         private OnDocumentosListener onDocumentosListener;
         private ViewHolder (View itemView, OnDocumentosListener onDocumentosListener){
             super(itemView);
             lblDescripcion = itemView.findViewById(R.id.lblDescripcion);
             lblPerfil = itemView.findViewById(R.id.lblPerfil);
-            btnDocumento = itemView.findViewById(R.id.btnDocumento);
+            //btnDocumento = itemView.findViewById(R.id.btnDocumento);
             btnAdvertencia = itemView.findViewById(R.id.btnAdvertencia);
             btnFavorito = itemView.findViewById(R.id.btnFavorito);
             btnOpcion = itemView.findViewById(R.id.btnOpcion);
             txtComentario = itemView.findViewById(R.id.txtComentario);
+            pdfView = itemView.findViewById(R.id.btnDocumento);
 
+            pdfView.setOnClickListener(this);
             lblDescripcion.setOnClickListener(this);
             lblPerfil.setOnClickListener(this);
-            btnDocumento.setOnClickListener(this);
+            //btnDocumento.setOnClickListener(this);
             btnAdvertencia.setOnClickListener(this);
             btnFavorito.setOnClickListener(this);
             btnOpcion.setOnClickListener(this);
@@ -180,6 +231,7 @@ public class DocumentosAdapter extends RecyclerView.Adapter<DocumentosAdapter.Vi
                 case R.id.btnDocumento:
                     onDocumentosListener.onDocumentosClick(getAdapterPosition(),documentosList,
                             Toast.makeText(context, "Esta es la miniatura", Toast.LENGTH_SHORT));
+                    onDocumentosListener.leerDocumento();
                     break;
                 case R.id.btnAdvertencia:
                     onDocumentosListener.reportarClick(getAdapterPosition(),documentosList);
@@ -205,5 +257,6 @@ public class DocumentosAdapter extends RecyclerView.Adapter<DocumentosAdapter.Vi
         void comentarioClick(int position, List<Documentos> list);
         void opcionClick(int position, List<Documentos> list);
         void agregarFavoritos(int adapterPosition, List<Documentos> documentosList);
+        void leerDocumento();
     }
 }
